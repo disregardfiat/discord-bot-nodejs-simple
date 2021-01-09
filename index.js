fa10ca08c95878a75293d40532bf44ae4163d2c7
Steven Ettinger <sjettingerjr@gmail.com>
2021-01-09 16:56:10 -0300

    fix hive

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ client.on('message', msg => {
                 return r.json()
             })
             .then(result => {
-                let ms = `DLUX is currently worth ${result.HiveVWMA.rate.toFixed(3)} HIVE, and ${result.HbdVWMA.rate.toFixed(3)} HIVE on the DEX}`
+                let ms = `DLUX is currently worth ${result.stats.HiveVWMA.rate.toFixed(3)} HIVE, and ${result.stats.HbdVWMA.rate.toFixed(3)} HIVE on the DEX}`
                 msg.channel.send(ms)
             })
             .catch(e => { console.log(e) })
