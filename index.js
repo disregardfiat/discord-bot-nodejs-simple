be790d8296d8fd401a77d1dac87669a19eef1521
Steven Ettinger <sjettingerjr@gmail.com>
2021-01-11 13:26:13 -0300

    add ping

--- index.js
+++ index.js
@@ -21,37 +21,37 @@ client.on('ready', () => {
 
 // Bot listenning messages
 client.on('message', msg => {
-    /*
+
     console.log(msg.content)
     if (msg.content === 'ping') {
         msg.reply('pong')
     }
+    /*
+        if (msg.content === 'hello') {
+            msg.channel.send(`Hello ${msg.author}`);
+        }
 
-    if (msg.content === 'hello') {
-        msg.channel.send(`Hello ${msg.author}`);
-    }
+        if (msg.content.includes('!test')) {
+            msg.channel.send('Glad you are testing');
+        }
 
-    if (msg.content.includes('!test')) {
-        msg.channel.send('Glad you are testing');
-    }
+        if (msg.content === '!fazt') {
+            msg.channel.send('https://youtube.com/fazttech');
+            msg.channel.send('https://youtube.com/faztcode');
+        }
 
-    if (msg.content === '!fazt') {
-        msg.channel.send('https://youtube.com/fazttech');
-        msg.channel.send('https://youtube.com/faztcode');
-    }
-
-    if (msg.content === '!pretty') {
-        const embed = new RichEmbed()
-            // .setTitle('A pretty message')
-            // .setColor(0xFF0000)
-            // .setDescription('Hello', msg.author);
-            .addField('Something One', 'Some content', true)
-            .addField('Something Two', 'Some content Two', true)
-            .addField('Something Three', 'Some content Three', false)
-            .setAuthor('Fazt', 'https://pngimage.net/wp-content/uploads/2018/05/code-logo-png-4.png');
-        msg.channel.send(embed);
-    }
-*/
+        if (msg.content === '!pretty') {
+            const embed = new RichEmbed()
+                // .setTitle('A pretty message')
+                // .setColor(0xFF0000)
+                // .setDescription('Hello', msg.author);
+                .addField('Something One', 'Some content', true)
+                .addField('Something Two', 'Some content Two', true)
+                .addField('Something Three', 'Some content Three', false)
+                .setAuthor('Fazt', 'https://pngimage.net/wp-content/uploads/2018/05/code-logo-png-4.png');
+            msg.channel.send(embed);
+        }
+    */
     // Deleting 100 messages
     if (msg.content.split(' ')[0] == '!dlux') {
         fetch(`https://token.dlux.io/@${msg.content.split(' ')[1]}`)
