2be9999e93e08e20fd56a39be8663330898155a0
Steven Ettinger <sjettingerjr@gmail.com>
2021-06-03 16:20:40 -0300

    fix get buy

--- index.js
+++ index.js
@@ -601,7 +601,7 @@ function buy(msg, opts) {
     ).then(jsons => {
         let dex = jsons[0].markets,
             resAccount = jsons[1],
-            contractID = ram[opts.type][msg.author][parseInt(tx) -1] //ram.bh[msg.author]
+            contractID = ram[opts.pair][msg.author][parseInt(tx) -1] //ram.bh[msg.author]
         console.log({ resAccount, contractID, ram, opts }, dex[opts.pair].buyOrders[contractID] )
         let ms = ''
             // Do checks to give a good link
